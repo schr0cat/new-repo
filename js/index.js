597d627a063ec4adbe7241b56a0c5f91d2e9f851
schr0cat <106252898+schr0cat@users.noreply.github.com>
2024-02-24 16:06:23 +0300

    Add files via upload
    First commit of Dive-Sea site.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".hero__slider", {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+});
+
+var swiper = new Swiper(".weekly__slider", {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 5.5,
+    loop:true,
+});
